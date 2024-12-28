6d32d32bef178a2df2c51d97df5e2a2b39ce095e
chindesu0207 <potter0207@gmail.com>
2024-12-29 00:11:32 +0800

    chore: 設置語系及 unsplash domains，上傳 product mock 檔

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="zh-tw">
       <body
         className={`${notoSansTC.variable} ${notoSansTC.className} antialiased`}
       >
